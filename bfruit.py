c3bff5030a1c03cab91b724a79c3b03a54fa81eb
Jesper Rasehorn <24442774+TheRealKettu@users.noreply.github.com>
2019-02-18 23:35:35 +0200

    Update bfruit.py

--- bfruit.py
+++ bfruit.py
@@ -1,6 +1,11 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+
 # Original bfruit made by:
 
 # Written by Balázs Nagy <nxbalazs@gmail.com>
